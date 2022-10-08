0bfef6547b82af37f30c5c1d540812bbbc66d7a3
Zheenbek Akimzhanov <zheenbek.akimzhanov@cloudera.com>
2022-10-08 22:01:05 +0200

    Add Date component

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
 import './App.css';
+import ExpenseItem from './components/ExpenseItem';
 
 function App() {
+  const expenses = [
+    {title: 'Car Insurance', amount: 274.23, date: new Date(2022, 2, 5)},
+    {title: 'Car Insurance2', amount: 232.23, date: new Date(2021, 12, 13)},
+    {title: 'Car Insurance3', amount: 254.23, date: new Date(2019, 4, 1)},
+    {title: 'Car Insurance4', amount: 174.23, date: new Date(2020, 5, 6)},
+  ]
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ExpenseItem 
+        date={expenses[0].date} 
+        title={expenses[0].title} 
+        amount={expenses[0].amount}
+      />
+      <ExpenseItem 
+        date={expenses[1].date} 
+        title={expenses[1].title} 
+        amount={expenses[1].amount}
+      />
+      <ExpenseItem 
+        date={expenses[2].date} 
+        title={expenses[2].title} 
+        amount={expenses[2].amount}
+      />
     </div>
   );
 }
--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,9 +1,15 @@
-function ExenseItem () {
+import './ExpenseItem.css'
+import ExpenseDate from './ExpenseDate';
+function ExpenseItem (props) {
     return (
-        <h2>
-            This is an Expense Item!
-        </h2>
+        <div className='expense-item'>
+            <ExpenseDate date={props.date} />
+            <div className='expense-item__description'>
+                <h2>{props.title}</h2>
+                <div className='expense-item__price'>{props.amount}</div>
+            </div>
+        </div>
     );
 }
 
-export default ExenseItem;
+export default ExpenseItem;
